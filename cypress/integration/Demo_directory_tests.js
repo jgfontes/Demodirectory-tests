59fcebb1495a87bc13cab4c763282bb2a3146669
jgfontes <ju.1996@outlook.com>
2021-01-14 01:43:23 -0300

    Added create advertisement and check notification

--- cypress/integration/Demo_directory_tests.js
+++ cypress/integration/Demo_directory_tests.js
@@ -1,4 +1,4 @@
-//Testing site access and login w/ the provided account
+//Test site access and login w/ the provided account
 describe ('Login and Access', function(){
     it ('Access website and login page', function(){
         cy.visit('https://demodirectory.com.br')
@@ -6,6 +6,7 @@ describe ('Login and Access', function(){
         cy.get('.nav-item-dropdown').within(() => {
             cy.get('[href="/sitemgr"]').click()
         })
+        cy.url().should('include', '/sitemgr/login.php')
     })
 
     it ('Login', function(){
@@ -16,23 +17,41 @@ describe ('Login and Access', function(){
             .clear()
             .type('abc123')
         cy.get('[type=submit]').click()
+        cy.url().should('include', '/sitemgr')
     })
 })
 
-//Testing advertisement's page and create itself
+//Test advertisement's page and create advertisement
 describe ('Create advertisement', function(){
     it('Access advertisement page', function(){
        cy.get('[id=tour-content]').click()
        cy.get('[id=dashboard-content]').contains('Anúncios').click()
        cy.get('[type=submit]').click()
+       cy.url().should('include', '/sitemgr/content/listing/')
     })
 
     it('Configure new advertisement', function(){
         cy.get('a.btn').contains('Adicionar anúncio').click()
         cy.get('[type = submit]').click()
+        cy.url().should('include', '/sitemgr/content/listing/listing.php')
         cy.get('[id = listingTemplate]').select('Restaurantes')
-        cy.get('[id=name]')
+        cy.get('[id = name]')
             .clear()
             .type('Pizzaria Gur')
+        cy.get('#browse-categories').click()
+        cy.get('div.categories-block.is-last')
+            .children()
+            .contains('Padaria')
+            .parents()
+            .find('a.addCategory')
+            .click()
+        cy.get('.col-xs-12 > .btn-primary').click()
+    })
+
+    //Read notification
+    it('Check created-w-success notification', function(){
+        cy.get('div.notify-item')
+            .should('have.attr', 'notify-type')
+            .and('include', 'success') 
     })
 })
