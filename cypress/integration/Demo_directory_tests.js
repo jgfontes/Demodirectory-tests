cecbe157372ac36dd4ba13c6d6ea4016efdb9d49
jgfontes <ju.1996@outlook.com>
2021-01-14 00:05:14 -0300

    Test advertesement page and create advertisement included

--- cypress/integration/Demo_directory_tests.js
+++ cypress/integration/Demo_directory_tests.js
@@ -1,8 +1,14 @@
 //Testing site access and login w/ the provided account
 describe ('Login and Access', function(){
     it ('Access website and login page', function(){
-        cy.visit('https://demodirectory.com.br/sitemgr/login.php')
-        cy.url().should('include', '/sitemgr/login.php')
+        cy.visit('https://demodirectory.com.br')
+        cy.get('.fa-users').click()
+        cy.get('.nav-item-dropdown').within(() => {
+            cy.get('[href="/sitemgr"]').click()
+        })
+    })
+
+    it ('Login', function(){
         cy.get('#username')
             .clear()
             .type('sitemgr@demodirectory.com')
@@ -10,5 +16,23 @@ describe ('Login and Access', function(){
             .clear()
             .type('abc123')
         cy.get('[type=submit]').click()
+    })
+})
+
+//Testing advertisement's page and create itself
+describe ('Create advertisement', function(){
+    it('Access advertisement page', function(){
+       cy.get('[id=tour-content]').click()
+       cy.get('[id=dashboard-content]').contains('Anúncios').click()
+       cy.get('[type=submit]').click()
+    })
+
+    it('Configure new advertisement', function(){
+        cy.get('a.btn').contains('Adicionar anúncio').click()
+        cy.get('[type = submit]').click()
+        cy.get('[id = listingTemplate]').select('Restaurantes')
+        cy.get('[id=name]')
+            .clear()
+            .type('Pizzaria Gur')
     })
 })
